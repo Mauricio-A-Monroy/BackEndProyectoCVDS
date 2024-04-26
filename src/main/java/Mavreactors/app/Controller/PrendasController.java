36cf9d7ab859ce62724d5a8421460ea3a69854b2
Mauricio Monroy <mauricio.monroy-j@mail.escuelaing.edu.co>
2024-04-25 20:55:48 -0500

    se esta arrelando :D

--- src/main/java/Mavreactors/app/Controller/PrendasController.java
+++ src/main/java/Mavreactors/app/Controller/PrendasController.java
@@ -1,5 +1,6 @@
 package Mavreactors.app.Controller;
 
+import Mavreactors.app.Model.Prendas;
 import Mavreactors.app.Service.PrendasService;
 import Mavreactors.app.dto.PrendasDto;
 import lombok.AllArgsConstructor;
@@ -13,7 +14,7 @@ import java.util.Base64;
 import java.util.List;
 
 @Controller
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:3000")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/prendas")
@@ -21,25 +22,10 @@ public class PrendasController {
 
     private final PrendasService prendasService;
 
-    @PostMapping
-    public ResponseEntity<PrendasDto> createPrenda(@RequestParam("imagen") MultipartFile file,
-                                                  @RequestBody PrendasDto prendasDto) {
-        try {
-            // Verificar si se ha seleccionado un archivo
-            if (!file.isEmpty()) {
-                // Convertir la imagen a una cadena Base64
-                String base64Image = Base64.getEncoder().encodeToString(file.getBytes());
-                // Asignar la imagen codificada a la DTO de prendas
-                prendasDto.setFoto(base64Image);
-            }
-
-            // Crear la prenda utilizando el servicio
-            PrendasDto savePrenda = prendasService.createPrenda(prendasDto);
-            return new ResponseEntity<>(savePrenda, HttpStatus.CREATED);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PostMapping("/subir-prenda")
+    public ResponseEntity<PrendasDto> createPrenda(@RequestBody PrendasDto prendasDto){
+        PrendasDto savePrenda = prendasService.createPrenda(prendasDto);
+        return new ResponseEntity<>(savePrenda, HttpStatus.CREATED);
     }
 
 
